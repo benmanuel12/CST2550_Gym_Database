c4ccc0b8676804c4fe1a39877dee03962bdcfda6
Benjamin Manuel <contact.ben.manuel@gmail.com>
2019-12-20 17:18:33 +0000

    wrote the basic functions code

--- JavaSQLQueries.java
+++ JavaSQLQueries.java
@@ -2,6 +2,10 @@ import java.sql.*;
 
 public class JavaSQLQueries {
 
+	Client testClient = new Client('C099', 'Test', 'Client');
+	Trainer testTrainer = new Trainer('T099', 'Test', 'Trainer');
+	Booking testBooking = new Booking('B099', 'C099', 'T099', '2019-12-25', '00:00:00', '02:00:00', 'weight loss');
+
     int trainerIndex = 6;
     int clientIndex = 11;
     int bookingIndex = 11;
@@ -9,25 +13,10 @@ public class JavaSQLQueries {
     public static void main(String[] args){
 	System.out.println("Program running");
     }
-
-    public static void basicTemplate(){
-	try {
-	    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
-
-	    String sqlQuery = "SELECT * FROM User";
-	    Statement statement = conn.createStatement();
-	    ResultSet results = statement.executeQuery(sqlQuery);
-
-	    while (results.next()){
-		System.out.println(results.getString(1) + " " + results.getString(2));
-	    }
-	} catch (SQLException ex) {
-	    System.out.println("SQL error: " + ex.getMessage());
-	}
-    }
     
-    // add a booking, assuming no clashes
-    public static void add(bookingDetails){
+	// add a booking, assuming no clashes
+	// check that bookingID doesn't exits
+    public static void add(Booking bookingDetails){
 	try {
 	    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
 
@@ -44,9 +33,9 @@ public class JavaSQLQueries {
 	    int qty = statement.executeUpdate(sql);
 	    System.out.println(qty + " records were updated");
 
-	    statement.close();
+		statement.close();
 	} catch (SQLException ex) {
-	    System.out.println("SQL error: " + ex.getMessage());
+		System.out.println("SQL error: " + ex.getMessage());
 	}
     }
 
@@ -55,13 +44,21 @@ public class JavaSQLQueries {
 		try {
 	    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
 
-	    String sqlQuery = "SELECT * FROM User";
+	    String sql = "SELECT * FROM Bookings;";
 	    Statement statement = conn.createStatement();
 	    ResultSet results = statement.executeQuery(sqlQuery);
 
 	    while (results.next()){
-		System.out.println(results.getString(1) + " " + results.getString(2));
-	    }
+		System.out.println(results.getString("bookingID") + " " 
+		+ results.getString("clientID") + " " 
+		+ results.getString("trainerID") + " " 
+		+ results.getString("dateBooked") + " " 
+		+ results.getString("timeStart") + " " 
+		+ results.getString("timeEnd") + " " 
+		+ results.getString("focus"));
+		}
+		
+		statement.close();
 	} catch (SQLException ex) {
 	    System.out.println("SQL error: " + ex.getMessage());
 	}
@@ -69,39 +66,112 @@ public class JavaSQLQueries {
     }
 
     // list all bookings for a given trainer
-    public static void listBookingTrainer(trainerID){
-	try{
-	} catch(SQLException ex) {
-	    System.out.println("SQL Error: " + ex.getMessage())
-	}
+    public static void listBookingTrainer(String trainerID){
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
+	
+			String sql = "SELECT * FROM Bookings WHERE trainerID = " + trainerID + ";";
+			Statement statement = conn.createStatement();
+			ResultSet results = statement.executeQuery(sqlQuery);
+	
+			while (results.next()) {
+			System.out.println(results.getString("bookingID") + " " 
+			+ results.getString("clientID") + " " 
+			+ results.getString("trainerID") + " " 
+			+ results.getString("dateBooked") + " " 
+			+ results.getString("timeStart") + " " 
+			+ results.getString("timeEnd") + " " 
+			+ results.getString("focus"));
+			}
+			
+			statement.close();
+		} catch (SQLException ex) {
+			System.out.println("SQL error: " + ex.getMessage());
+		}
     }
 
     // list all bookings for a given client
-    public static void listBookingClient(clientID){
-	try{
-	} catch(SQLException ex) {
-	    System.out.println("SQL Error: " + ex.getMessage())
-	}
+    public static void listBookingClient(String clientID){
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
+	
+			String sql = "SELECT * FROM Bookings WHERE clientID = " + clientID + ";";
+			Statement statement = conn.createStatement();
+			ResultSet results = statement.executeQuery(sqlQuery);
+	
+			while (results.next()) {
+			System.out.println(results.getString("bookingID") + " " 
+			+ results.getString("clientID") + " " 
+			+ results.getString("trainerID") + " " 
+			+ results.getString("dateBooked") + " " 
+			+ results.getString("timeStart") + " " 
+			+ results.getString("timeEnd") + " " 
+			+ results.getString("focus"));
+			}
+			
+			statement.close();
+		} catch (SQLException ex) {
+			System.out.println("SQL error: " + ex.getMessage());
+		}
     }
 
     // list all bookings for a given day
-    public static void listBookingDate(date){
-	try{
-	} catch(SQLException ex) {
-	    System.out.println("SQL Error: " + ex.getMessage())
-	}
+    public static void listBookingDate(String date){
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
+	
+			String sql = "SELECT * FROM Bookings WHERE dateBooked = " + date + ";";
+			Statement statement = conn.createStatement();
+			ResultSet results = statement.executeQuery(sqlQuery);
+	
+			while (results.next()){
+			System.out.println(results.getString("bookingID") + " " 
+			+ results.getString("clientID") + " " 
+			+ results.getString("trainerID") + " " 
+			+ results.getString("dateBooked") + " " 
+			+ results.getString("timeStart") + " " 
+			+ results.getString("timeEnd") + " " 
+			+ results.getString("focus"));
+			}
+			
+			statement.close();
+		} catch (SQLException ex) {
+			System.out.println("SQL error: " + ex.getMessage());
+		}
     }
 
     // update a specific booking defined by its id
-    public static void update(bookingID, bookingDetails){
+    public static void update(String bookingID, Booking bookingDetails){
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
 	
+			String sql = "UPDATE Bookings SET bookingID = " + bookingDetails.getId() + ", clientID = "  + bookingDetails.getClientId() + ", trainerID = "  + bookingDetails.getTrainerId() + ", dateBooked = "  + bookingDetails.getDate() + ", timeStart = "  + bookingDetails.getTimeStart() + ", timeEnd = "  + bookingDetails.getTimeEnd() + ", focus = "  + bookingDetails.getFocus() + "WHERE bookingID = " + bookingID + ";";
+			Statement statement = conn.createStatement();
+			ResultSet results = statement.executeQuery(sqlQuery);
+
+			int qty = statement.executeUpdate(sql);
+			System.out.println(qty + " records were updated");
+
+			statement.close();
+		} catch (SQLException ex) {
+			System.out.println("SQL error: " + ex.getMessage());
+		}
     }
 
     // delete a specific booking defined by its id
-    public static void deleteBooking(clientID, trainerID, date){
-	try{
-	} catch(SQLException ex) {
-	    System.out.println("SQL Error: " + ex.getMessage())
-	}
-    }
+    public static void deleteBooking(String bookingID){
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gymBookings", "root", "");
+	
+			String sqlQuery = "DELETE FROM Bookings WHERE bookingID = " + bookingID + ";";
+			Statement statement = conn.createStatement();
+			ResultSet results = statement.executeQuery(sqlQuery);
+		
+			int qty = statement.executeUpdate(sql);
+			System.out.println(qty + " records were updated");
+
+			statement.close();
+		} catch (SQLException ex) {
+			System.out.println("SQL error: " + ex.getMessage());
+		}
 }
